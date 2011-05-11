a8ff028e1103c200f8e43872f1f97f4e73471d24
Pablo Resines <pablo@soporteya.com>
2011-05-11 17:27:51 -0500

    Se copió el theme de la version anterior. Se modificó la vista en contacto por incompatibilad con el nuevo módulo. Se actualizaron algunas partes del CSS para compatibilidad con v1.2

---
+++ addons/themes/sustentabilis/theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Sustentabilis extends Theme {
+
+    public $name = 'Sustentabilis';
+    public $author = 'Pablo Resines';
+    public $author_website = 'http://www.palmea.com/';
+    public $website = 'http://www.sustentabilis.com.mx/';
+    public $description = 'Custom Template';
+    public $version = '1.0 beta';
+
+}
+
+/* End of file theme.php */
